d3be0addbf4d4721a77163b7f48b0666f1995221
EnizBalihodzic <157683140+EnizBalihodzic@users.noreply.github.com>
2024-02-07 08:33:06 +0100

    Add files via upload

---
+++ test6.py
@@ -0,0 +1,24 @@
+import unittest
+from tkinter import Label
+from test1 import finish_reg
+
+#Test ukoliko pokusamo kreirati racun sa jednim praznim poljem
+class TestFinishRegFunction(unittest.TestCase):
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+    def test_finish_reg_uspjesno(self):
+        temp_name = "Elektrotehnicki"
+        temp_lastname = "Etfovic"
+        temp_age = ""
+        temp_gender = "Muski"
+        temp_password = "password123"
+        temp_status = "Zaposlen"
+        temp_email = ""
+        notif = Label()
+        finish_reg(temp_name, temp_lastname, temp_age, temp_gender, temp_password, temp_status, temp_email, notif)
+        expected_text = "Sva polja morau biti popunjena! "
+        self.assertEqual(notif.cget("text"), expected_text)
+if __name__ == '__main__':
+    unittest.main()
